cd06d66a1b9faeb7148b1b0e563cf1b07d70c74f
xowjd <xowjd@LAPTOP-8BFLMA2D>
2021-08-02 23:08:02 +0900

    21/08/02

--- HELLOWORLD/src/buffer.java
+++ HELLOWORLD/src/buffer.java
@@ -12,10 +12,12 @@ public class buffer {
 		StringTokenizer st ; //특정 문자에 따라 문자열을 나누고 싶을 때 사용한다.
 		int t = Integer.parseInt(br.readLine()); // BufferedReader는 항상 String으로 받으므로 입력값을 정수로 사용하려면 형변환이 필요하다
 		for (int a = 0 ; a<t ; a++) {
-		st = new StringTokenizer(br.readLine());// anj
+		st = new StringTokenizer(br.readLine());
 		bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n" );
 		}
 		bw.close();
 	}
 
 }
+
+
