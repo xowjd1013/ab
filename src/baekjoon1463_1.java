1ee53a643be6e8ededc6ad36d5769c3bab8a56ab
TaeJeong <xowjd1013@naver.com>
2022-02-27 22:01:21 +0900

    백준 1463 - 1로 만들기 [재귀, DP]

--- src/baekjoon1463_1.java
+++ src/baekjoon1463_1.java
@@ -2,28 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// Top-Down
+/*
+ 동적 프로그래밍(Dynamic Programming) : 재귀 호출 시, 반복적으로 계산되는 것들의 계산 횟수를 줄이기 위해 이전에 계산했던 값을 저장해두었다가 나중에 재사용하는 방법이다. 
+ 									메모이제이션이 동적 프로그래밍 중 하나.
+*/
+
+
+// Bottom-up
 public class baekjoon1463_1 {
 	static int ans = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
 		int n = Integer.parseInt(br.readLine());
-		
-		calcul(n,0);
-		System.out.println(ans);
+		int []dp = new int [n+1];
+		dp[0] = 0;
+		dp[1] = 0;
+		for(int i = 2; i<=n; i++) {
+			dp[i] = dp[i-1] +1;
+			if(i%2==0) dp[i]=Math.min(dp[i],dp[i/2]+1);
+			if(i%3==0) dp[i]=Math.min(dp[i],dp[i/3]+1);
+		}
+		System.out.println(dp[n]);
+	
 	}
-	public static void calcul(int n, int cnt) {
-		if(n == 1) {
-			ans = Math.min(ans, cnt);
-			return;
-		}
-		if(cnt>=ans) return;
-		if(n % 3 == 0) {
-			calcul(n/3,cnt+1);
-		}
-		if(n % 2 == 0) {
-			calcul(n/2,cnt+1);
-		}
-		calcul(n-1,cnt+1);
-	}
+	/* 바텀업 함수 정의{
+		1. 동적 배열 생성
+		2. for(2~num)까지 돌면서 dp[]의 인덱스 값마다 최소 cnt를 계산
+			2-1. dp[i] = dp[i-1] +1
+			2-2. 2로 나누어 떨어졌을때, 3으로 나누어 떨어졌을때 최소값 각각 비교
+		3. dp[num]을 리턴 
+	*/
+	
+	
+	
 }
---
+++ src/baekjoon1463_2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Top-Down
+public class baekjoon1463_2 {
+	static int ans = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		calcul(n,0);
+		System.out.println(ans);
+	}
+	public static void calcul(int n, int cnt) {
+		if(n == 1) {
+			ans = Math.min(ans, cnt);
+			return;
+		}
+		if(cnt>=ans) return;
+		if(n % 3 == 0) {
+			calcul(n/3,cnt+1);
+		}
+		if(n % 2 == 0) {
+			calcul(n/2,cnt+1);
+		}
+		calcul(n-1,cnt+1);
+	}
+}
